8f8256a7f455911b2b96c5522a7074e8918cd8fa
LEGION\prabh <prabhjotsingh394@gmail.com>
2020-10-25 15:52:53 +0530

    commit 3

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-const express = require('express'); const path = require('path'); const app = express(); 
-// Serve static files....
-app.use(express.static(__dirname + '/dist/sapcex-launch-program')); 
-// Send all requests to index.html
-app.get('/*', function(req, res) { res.sendFile(path.join(__dirname + '/dist/sapcex-launch-program/index.html')); }); 
-// default Heroku PORT 
-app.listen(process.env.PORT || 3000);
+// const express = require('express'); const path = require('path'); const app = express(); 
+// // Serve static files....
+// app.use(express.static(__dirname + '/dist/sapcex-launch-program')); 
+// // Send all requests to index.html
+// app.get('/*', function(req, res) { res.sendFile(path.join(__dirname + '/dist/sapcex-launch-program/index.html')); }); 
+// // default Heroku PORT 
+// app.listen(process.env.PORT || 3000);
+
+const express = require('express');
+const app = express();
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
